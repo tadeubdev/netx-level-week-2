531905f14d2957c24175f16139c853a2fd95fd88
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-10-15 10:13:43 -0300

    remove mobile como submodule

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function OrphanageDetails() {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
+export default OrphanageDetails;
